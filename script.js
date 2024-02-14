bc00c0e3fe9304c0b5c2c132345d970e20769517
Sh1v4nk <shivankpandey113@gmail.com>
2024-02-14 18:59:06 +0530

    Enhance operator handling in JavaScript code

--- script.js
+++ script.js
@@ -1,29 +1,38 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const resultElement = document.querySelector(".result");
-  const buttons = document.querySelectorAll(".btn");
+const resultElement = document.querySelector(".result");
+const buttons = document.querySelectorAll(".btn");
 
-  buttons.forEach((button) => {
-    button.addEventListener("click", function () {
-      const buttonText = button.innerText;
+buttons.forEach((button) => {
+  button.addEventListener("click", function () {
+    const buttonText = button.innerText;
+    const lastChar = resultElement.value.slice(-1);
 
-      switch (buttonText) {
-        case "AC":
-          resultElement.value = "";
-          break;
-        case "DEL":
-          resultElement.value = resultElement.value.slice(0, -1);
-          break;
-        case "=":
-          try {
-            resultElement.value = eval(resultElement.value);
-          } catch (error) {
-            resultElement.value = "Error";
-          }
-          break;
-        default:
+    switch (buttonText) {
+      case "AC":
+        resultElement.value = "";
+        break;
+      case "DEL":
+        resultElement.value = resultElement.value.slice(0, -1);
+        break;
+      case "=":
+        try {
+          resultElement.value = eval(resultElement.value);
+        } catch (error) {
+          resultElement.value = "Error";
+        }
+        break;
+      case "+":
+      case "-":
+      case "*":
+      case "/":
+        if (["+", "-", "*", "/"].includes(lastChar)) {
+          resultElement.value = resultElement.value.slice(0, -1) + buttonText;
+        } else {
           resultElement.value += buttonText;
-          break;
-      }
-    });
+        }
+        break;
+      default:
+        resultElement.value += buttonText;
+        break;
+    }
   });
 });
